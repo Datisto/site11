25dbd946af57bd3d1ac7c129c46b554f4665e0d4
nickolanico900-a11y <nickolanico900@gmail.com>
2025-11-14 21:32:38 +0200

    Updated Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -115,7 +115,7 @@ const Hero = () => {
               </div>
               
               <div className="mb-4 lg:mb-6">
-                <div className="flex items-center gap-4 mb-2">
+                <div className="flex flex-col gap-1 mb-2">
                   <span className="line-through text-lg lg:text-xl text-gray-500 font-montserrat">190 €</span>
                   <span className="font-bold text-3xl lg:text-4xl font-montserrat text-lime-600">12 €</span>
                 </div>
