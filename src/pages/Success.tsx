c53fbf1057bf3481e6c3dedf14c8dcda5a84fc7c
Datisto <nickyettikiller@gmail.com>
2025-12-04 19:55:19 +0200

    Updated Success.tsx

--- src/pages/Success.tsx
+++ src/pages/Success.tsx
@@ -7,7 +7,7 @@ export default function Success() {
     localStorage.removeItem('paymentTimestamp');
 
     if (typeof window !== 'undefined' && (window as any).fbq) {
-      (window as any).fbq('track', 'Purchase', {
+      (window as any).fbq('track', 'InitiateCheckout', {
         value: 12.00,
         currency: 'EUR'
       });
